c52f857a3e50c67fda02a8d8bd8631e1757278cc
Kirill Rozhin <k-rozhin@mail.ru>
2022-04-15 17:49:47 +0300

    Add project files.

---
+++ WareApp/AddEditPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WareApp
+{
+    /// <summary>
+    /// Interaction logic for AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Item _currentItem = new Item();
+        public AddEditPage(Item selectedItem)
+        {
+            InitializeComponent();
+
+            if (selectedItem != null)
+                _currentItem = selectedItem;
+
+            DataContext = _currentItem;
+            ComboWare.ItemsSource = WareBaseEntities.GetContext().Warehouses.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentItem.id == 0)
+                WareBaseEntities.GetContext().Items.Add(_currentItem);
+
+            try
+            {
+                WareBaseEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена!");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
