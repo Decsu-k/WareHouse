2bfc529aa8bf3a22cca14cd818becde916c58b7a
Decsu-k <82495296+Decsu-k@users.noreply.github.com>
2022-06-02 22:29:33 +0300

    UnitTest

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            User oUser = new User();
+            Assert.AreEqual(true, oUser.LoginScreen("User1", "123"), "admin/admin should be a good login");
+        }
+    }
+}
